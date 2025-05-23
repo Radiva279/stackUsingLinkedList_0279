14d4722c22aa534f64500ef579a7ef7adc1dc975
Radiva279 <radivagalih12@gmail.com>
2025-05-23 08:39:16 +0700

    menambahkan else pada prosedur peek

--- stack.cpp
+++ stack.cpp
@@ -58,3 +58,14 @@ public:
         {
             cout << "List is empty. " << endl;
         }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        } // Return the value of the top node
+    }
