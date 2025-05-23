1c6a50a7a0f6745ef8c41323907d2f0be3ef0be6
Radiva279 <radivagalih12@gmail.com>
2025-05-23 08:41:35 +0700

    menambahkan perulangan while

--- stack.cpp
+++ stack.cpp
@@ -84,3 +84,12 @@ int main()
     int choice = 0;
     int value;
 
+    while (choice != 4)
+    {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peep\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
