a2ee089370b7261c6795ac4f2b7c81faebd8d4f0
Radiva279 <radivagalih12@gmail.com>
2025-05-23 08:34:53 +0700

    alokasi memory dan  menetapkan nilai

--- stack.cpp
+++ stack.cpp
@@ -28,3 +28,6 @@ public:
 
     // Push opertaion: Insert an element onto the top of the stack
     int push(int value)
+    {
+        Node *newNode = new Node(); // 1.Allocate memory for the new node
+        newNode->data = value;      // 2. assign value
